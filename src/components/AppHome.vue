5757ee7c0235556cabcec879652dfaee6797ccc6
jagelvisr <josegagelvis@gmail.com>
2019-05-13 20:02:52 -0400

    mejoras de mensaje de alerta en contact y agregar formato app scroll responsive desing

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,37 +1,75 @@
 <template>
     <section id="Home">
-        <v-navigation-drawer app v-model="drawer" class="grey darken-4" dark>
-            <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
-                        <v-list-tile-content v-if="item.title != 'Contact' ">
-                            {{item.title}}
-                        </v-list-tile-content>
-                        <v-list-tile-content v-else color="red darken-4">
-                            {{item.title}}
-                        </v-list-tile-content>
-                    </v-list-tile>
-                    <v-divider :key="`divider-${index}`"></v-divider>
-                </template>
+        <v-navigation-drawer 
+        app 
+        v-model="drawer" 
+        class="grey darken-4" 
+        dark 
+        temporary
+        width="200"
+        >
+            <v-list class="pa-1 mt-2">
+                    <v-list-tile avatar>
+                        <v-list-tile-avatar>
+                            <img :src="require('@/assets/jagelvisR.jpeg')" height="900">
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{appTitle}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-divider></v-divider>
+            </v-list>
+                
+            <v-list
+                v-for="item in items"
+                :key="item.title"
+                class="pt-0" 
+                dense
+                >
+                <scrollactive 
+                    class="my-nav" 
+                    active-class="active"
+                    :offset="80"
+                    :duration="900"
+                    bezier-easing-value=".5,0,.35,1">
+                    
+                    <v-btn 
+                        dark
+                        :color="`${item.color}`" 
+                        class="scrollactive-item nav-item" 
+                        :href="`${item.scroll}`" 
+                        @click="drawer = !drawer"
+                    >
+                        {{ item.title }}
+                    </v-btn> 
+                </scrollactive>
             </v-list>
         </v-navigation-drawer>
         <!--Scroll para ONEPAGE HOME con Scrollactive-->
         <scrollactive 
-        class="my-nav" 
-        active-class="active"
-        :offset="80"
-        :duration="900"
-        bezier-easing-value=".5,0,.35,1">
-            <v-toolbar app color="grey darken-4" dark class="white">
+            class="my-nav" 
+            active-class="active"
+            :offset="80"
+            :duration="900"
+            bezier-easing-value=".5,0,.35,1"
+        >
+            <v-toolbar 
+            app 
+            color="grey darken-4" 
+            dark 
+            class="white" >
                 <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
                 <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+
                 <v-spacer class="hidden-md-and-up"></v-spacer>
                 <v-spacer class="hidden-sm-and-down"></v-spacer>
+
                 <v-btn flat class="hidden-sm-and-down scrollactive-item nav-item" href="#Home">Home</v-btn>
                 <v-btn flat class="hidden-sm-and-down scrollactive-item" href="#AboutMe">About Me</v-btn>
                 <v-btn flat class="hidden-sm-and-down scrollactive-item" href="#Services">Services</v-btn>
                 <v-btn flat class="hidden-sm-and-down scrollactive-item" href="#Portfolio">Portfolio</v-btn>
-                <v-btn color="red darken-3" class="hidden-sm-and-down scrollactive-item" href="#ContactMe">Contact Me</v-btn>
+                <v-btn color="red darken-4" class="hidden-sm-and-down scrollactive-item" href="#ContactMe">Contact Me</v-btn>
             </v-toolbar>
         </scrollactive>
     </section>
@@ -46,11 +84,11 @@ export default {
             appTitle: 'José Agelvis',
             drawer: false,
             items: [
-                { title: 'Home' },
-                { title: 'About Me' },
-                { title: 'Services' },
-                { title: 'Porfolio' },
-                { title: 'Contact Me' }
+                { title: 'Home', scroll: '#Home', color: 'grey darken-4'},
+                { title: 'About Me', scroll: '#AboutMe', color: 'grey darken-4' },
+                { title: 'Services', scroll: '#Services', color: 'grey darken-4' },
+                { title: 'Portfolio', scroll: '#Portfolio', color: 'grey darken-4' },
+                { title: 'Contact Me', scroll: '#ContactMe', color: 'red darken-4'  }
             ],
         }   
     },   

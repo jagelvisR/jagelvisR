c7417b05b69e9a54df112643198da5e2c7939115
José Agelvis <josegagelvis@gmail.com>
2019-07-03 12:41:36 -0400

    eliminacion de libreria vue-backtoto y utilizacion de backtoscroll de vuetify con setTime de retorno

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,19 @@
   <v-app id="app" app dark>
         <v-content transition="slide-x-transition">
             <router-view></router-view>
-            <back-to-top 
-                visibleoffset="150"
-                bottom="75px" 
-                right="05px"
-                >
-                <v-btn
-                    id="lateral"
-                    justify-center 
-                    wrap
-                    icon
-                    dark
-                    bottom
-                    fab
-                    class="red darken-3 btn-to-top"
-                    
-                    >
-                        <v-icon large color="white">keyboard_arrow_up</v-icon>
-                </v-btn>
-            </back-to-top>
+            <v-btn
+              v-scroll="onScroll"
+              v-show="fab"
+              fab
+              dark
+              fixed
+              bottom
+              right
+              color="red darken-3"
+              @click="toTop"
+            >
+              <v-icon>keyboard_arrow_up</v-icon>
+          </v-btn>
         </v-content>
     </v-app>
 </template>
@@ -31,30 +25,22 @@
 
 export default {
   name: 'App',
-  components :{
+  data: () => ({
+    fab: false
+  }),
+  methods: {
+    onScroll (e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset ||   e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    toTop () {
+      this.$vuetify.goTo("#Home", { duration: 900 });
+    }
   }
 };
 </script>
 
 <style scoped>
-
-/* css btn scroll */
-.btn-to-top {
-  width: 60px;
-  height: 60px;
-  padding: 10px 16px;
-  border-radius: 50%;
-  font-size: 22px;
-  line-height: 22px;
-}
-
-/* This is for documentation purposes and will not be needed in your application */
-  #lateral .v-speed-dial,
-  #lateral .v-btn--floating {
-    position: absolute;
-  }
-  #lateral .v-btn--floating {
-    margin: 0 0 16px 16px;
-  }
 
 </style>
